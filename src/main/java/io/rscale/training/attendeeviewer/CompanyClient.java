f72458996a119b5f808cfbe01b469d875fa9204c
jcabuhat <JCabuhat@sandiego.gov>
2017-09-14 13:36:26 -0700

    commit

--- src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
+++ src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
@@ -1,13 +1,14 @@
 package io.rscale.training.attendeeviewer;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient("company")
 public interface CompanyClient {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/company")
-	Company getCompany(String id);
+	@RequestMapping(method = RequestMethod.GET, value = "companies/{companyId}")
+	Company getCompany(@PathVariable("companyId") String id);
 	
 }
